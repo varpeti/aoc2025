1104f6a041d41cc6c6115c2ecc30930875d5dfb9
varpeti <peti.var97@gmail.com>
2025-12-04 22:46:33 +0100

    days

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,5 +1,12 @@
-#[allow(dead_code)]
-pub fn a() {}
+#[allow(unused_imports)]
+use anyhow::{Result, anyhow};
 
-#[allow(dead_code)]
-pub fn b() {}
+#[allow(dead_code, unused_variables)]
+pub fn a(input: &str) -> Result<String> {
+    Ok(format!("{}", 0x70D0))
+}
+
+#[allow(dead_code, unused_variables)]
+pub fn b(input: &str) -> Result<String> {
+    Ok(format!("{}", 0x70D0))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod days;
-
 use crate::days::*;
+use anyhow::Result;
+use std::fs;
 
-fn main() {
-    day01::a();
-    day01::b();
+#[allow(dead_code, unused_macros)]
+macro_rules! day {($n:literal, $p:ident) => {paste::paste!{println!("{}",[<day $n>]::$p(&String::from_utf8(fs::read(concat!("inputs/day", stringify!($n), ".txt"))?)?)?)}};}
+
+#[allow(dead_code, unused_macros)]
+macro_rules! tst {($n:literal, $p:ident) => {paste::paste!{println!("{}",[<day $n>]::$p(&String::from_utf8(fs::read(concat!("inputs/tst", stringify!($n), ".txt"))?)?)?)}};}
+
+fn main() -> Result<()> {
+    day!(01, b);
+    Ok(())
 }
