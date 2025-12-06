6c385e8f338f0a0847a19651f7235b01a90d7bb8
varpeti <peti.var97@gmail.com>
2025-12-07 00:40:57 +0100

    day04 a

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,9 +1,75 @@
 #[allow(unused_imports)]
 use anyhow::{Result, anyhow};
 
+#[derive(Debug, PartialEq, Eq)]
+enum Cell {
+    Empty,
+    Paper,
+}
+impl Cell {
+    fn from_char(c: char) -> Result<Self> {
+        Ok(match c {
+            '.' => Cell::Empty,
+            '@' => Cell::Paper,
+            oth => return Err(anyhow!("Expected '.' or '@' but got `{}`", oth)),
+        })
+    }
+}
+
+type Room = Vec<Vec<Cell>>;
+
+fn get_neighbours(room: &Room, y: usize, x: usize) -> Vec<&Cell> {
+    let mut neighbours = Vec::new();
+    for iy in -1..=1 {
+        for ix in -1..=1 {
+            if iy == 0 && ix == 0 {
+                continue;
+            }
+            let y = y as isize + iy;
+            if y < 0 || y >= room.len() as isize {
+                continue;
+            }
+            let x = x as isize + ix;
+            if x < 0 || x >= room[0].len() as isize {
+                continue;
+            }
+            neighbours.push(&room[y as usize][x as usize])
+        }
+    }
+    neighbours
+}
+
 #[allow(dead_code, unused_variables)]
 pub fn a(input: &str) -> Result<String> {
-    Ok(format!("{}", 0x70D0))
+    let room: Room = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .filter_map(|c| Cell::from_char(c).ok())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut sum = 0;
+
+    for y in 0..room.len() {
+        for x in 0..room[0].len() {
+            if room[y][x] != Cell::Paper {
+                continue;
+            }
+            if room[y][x] == Cell::Paper
+                && get_neighbours(&room, y, x)
+                    .into_iter()
+                    .filter(|c| **c == Cell::Paper)
+                    .count()
+                    < 4
+            {
+                sum += 1;
+            }
+        }
+    }
+
+    Ok(format!("{}", sum))
 }
 
 #[allow(dead_code, unused_variables)]
