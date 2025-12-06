50096c90d2e7730c285bf2054ea18de5cac171bc
varpeti <peti.var97@gmail.com>
2025-12-06 21:35:22 +0100

    day3 b

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -22,14 +22,31 @@ pub fn a(input: &str) -> Result<String> {
         if last > max[1] {
             max[1] = last;
         }
-
         sum += max[0].to_digit(10).unwrap() * 10 + max[1].to_digit(10).unwrap();
     }
-
     Ok(format!("{}", sum))
 }
 
 #[allow(dead_code, unused_variables)]
 pub fn b(input: &str) -> Result<String> {
-    Ok(format!("{}", 0x70D0))
+    let mut sum = 0u64;
+    for bank in input.lines() {
+        let mut s = 0usize;
+        let mut e = bank.len() - 11;
+        let mut max = ['0'; 12];
+        for m in max.iter_mut() {
+            let mut next_s = s + 1;
+            for i in s..e {
+                let c = bank.as_bytes()[i] as char;
+                if c > *m {
+                    *m = c;
+                    next_s = i + 1;
+                }
+            }
+            s = next_s;
+            e += 1;
+        }
+        sum += max.into_iter().collect::<String>().parse::<u64>().unwrap();
+    }
+    Ok(format!("{}", sum))
 }
