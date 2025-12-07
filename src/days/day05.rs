c916d536428fc92ea9a53535ba70b2ccefcefc34
varpeti <peti.var97@gmail.com>
2025-12-07 20:04:10 +0100

    day05 b

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 #[allow(unused_imports)]
 use anyhow::{Result, anyhow};
 
@@ -33,5 +35,29 @@ pub fn a(input: &str) -> Result<String> {
 
 #[allow(dead_code, unused_variables)]
 pub fn b(input: &str) -> Result<String> {
-    Ok(format!("{}", 0x70D0))
+    let mut ranges = Vec::<RangeInclusive<u64>>::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            break;
+        }
+        let (s, e) = line.split_once('-').unwrap();
+        let mut s = s.parse::<u64>()?;
+        let mut e = e.parse::<u64>()?;
+        ranges.retain(|range| {
+            if s <= *range.end() && *range.start() <= e {
+                s = u64::min(s, *range.start());
+                e = u64::max(e, *range.end());
+                false
+            } else {
+                true
+            }
+        });
+        ranges.push(s..=e);
+    }
+
+    let mut sum = 0u64;
+    for range in ranges {
+        sum += range.end() - range.start() + 1;
+    }
+    Ok(format!("{}", sum))
 }
