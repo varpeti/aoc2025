fd27e7045371660dbfe973bd9c2a3f2a5b86a24a
varpeti <peti.var97@gmail.com>
2025-12-07 19:15:47 +0100

    day05 a

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -3,7 +3,32 @@ use anyhow::{Result, anyhow};
 
 #[allow(dead_code, unused_variables)]
 pub fn a(input: &str) -> Result<String> {
-    Ok(format!("{}", 0x70D0))
+    let mut ranges = Vec::new();
+    let mut sum = 0u32;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
+        match line.split_once('-') {
+            Some((s, e)) => {
+                let s = s.parse::<u64>()?;
+                let e = e.parse::<u64>()?;
+                ranges.push(s..=e);
+            }
+            None => {
+                let id = line.parse::<u64>()?;
+                for range in ranges.iter() {
+                    if range.contains(&id) {
+                        sum += 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(format!("{}", sum))
 }
 
 #[allow(dead_code, unused_variables)]
