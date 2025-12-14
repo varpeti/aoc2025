5d93faafcc275a118b46ed434cd2c0f6f594cd1d
varpeti <peti.var97@gmail.com>
2025-12-14 18:58:15 +0100

    day11 b

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -20,5 +20,23 @@ pub fn a(input: &str) -> Result<String> {
 
 #[allow(dead_code, unused_variables)]
 pub fn b(input: &str) -> Result<String> {
-    Ok(format!("{}", 0x70D0))
+    let mut nodes = Nodes::new();
+    for line in input.lines() {
+        let (from, to) = line.split_once(':').unwrap();
+        let to = to.split_whitespace().collect::<Vec<&str>>();
+        nodes.insert(from, to);
+    }
+    nodes.insert("out", vec![]);
+    let dac_fft = count_paths("dac", |&pos| nodes[pos].clone(), |&pos| pos == "fft");
+    let sum = if dac_fft != 0 {
+        let svr_dac = count_paths("svr", |&pos| nodes[pos].clone(), |&pos| pos == "dac");
+        let fft_out = count_paths("fft", |&pos| nodes[pos].clone(), |&pos| pos == "out");
+        svr_dac * dac_fft * fft_out
+    } else {
+        let fft_dac = count_paths("fft", |&pos| nodes[pos].clone(), |&pos| pos == "dac");
+        let svr_fft = count_paths("svr", |&pos| nodes[pos].clone(), |&pos| pos == "fft");
+        let dac_out = count_paths("dac", |&pos| nodes[pos].clone(), |&pos| pos == "out");
+        svr_fft * fft_dac * dac_out
+    };
+    Ok(format!("{}", sum))
 }
