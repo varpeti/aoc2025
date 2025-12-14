ed57e4beae045abcf0b6f9d8bfc667b00ebbe187
varpeti <peti.var97@gmail.com>
2025-12-14 18:35:04 +0100

    day11 a

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,9 +1,21 @@
+use std::collections::BTreeMap;
+
 #[allow(unused_imports)]
 use anyhow::{Result, anyhow};
+use pathfinding::prelude::count_paths;
+
+type Nodes<'a> = BTreeMap<&'a str, Vec<&'a str>>;
 
 #[allow(dead_code, unused_variables)]
 pub fn a(input: &str) -> Result<String> {
-    Ok(format!("{}", 0x70D0))
+    let mut nodes = Nodes::new();
+    for line in input.lines() {
+        let (from, to) = line.split_once(':').unwrap();
+        let to = to.split_whitespace().collect::<Vec<&str>>();
+        nodes.insert(from, to);
+    }
+    let sum = count_paths("you", |&pos| nodes[pos].clone(), |&pos| pos == "out");
+    Ok(format!("{}", sum))
 }
 
 #[allow(dead_code, unused_variables)]
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,6 @@ macro_rules! day {($n:literal, $p:ident) => {paste::paste!{println!("{}",[<day $
 macro_rules! tst {($n:literal, $p:ident) => {paste::paste!{println!("{}",[<day $n>]::$p(&String::from_utf8(fs::read(concat!("inputs/tst", stringify!($n), ".txt"))?)?)?)}};}
 
 fn main() -> Result<()> {
-    day!(10, b);
+    day!(11, a);
     Ok(())
 }
